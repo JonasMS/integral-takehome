1d5b2f1fb59ec8a9007b75af77f9d07ba7177d71
JonasMS <jonas.sota@gmail.com>
2022-03-17 12:40:25 -0600

    Misc fixes - add comments - add some security features

--- contracts/QuestionH.sol
+++ contracts/QuestionH.sol
@@ -16,6 +16,7 @@ contract QuestionH {
     }
 
     mapping(uint => Job) jobs;
+    bool executing;
 
     event JobSubmitted(address indexed submitter, uint indexed jobId, address target, uint value, bytes calldatas, uint delay);
     event JobExecuted(address indexed executer, uint indexed jobId);
@@ -31,15 +32,30 @@ contract QuestionH {
     }
 
     /**
-        * Submit a list of jobs
-        * Each job specifies a contract address and a method to call
-        * Each job can only be executed after the specified delay
-        * Jobs are executed in exchanged for ETH
+        * @dev Submits a job
+        *
+        * Each job specifies a contract address and a method to call + associated call data,
+        * and how much time has to pass before the job can be executed (i.e. 'delay').
+        * 
+        * Jobs are executed in exchanged for ETH, the amount of which is determined by the
+        * amount of ETH deposited to the contract via this transaction.
      */
-    function submitJob(address target, uint value, bytes calldatas, uint delay) external payable {
-        // validate params
-        
-        // get job id by hashing job
+    function submitJob(address target, uint value, bytes calldatas, uint delay) external payable {        
+        /**
+            * @dev Prevent jobs that would have this contract create jobs with the bounty
+            * coming from this contract's funds.
+            *
+            * This exploit could be used to drain the contract's funds.
+         */
+        require(target !== address(this), "QuestionH::submitJob: INVALID_TARGET");
+
+        /**
+            * @dev Get job id by hashing job
+            *
+            * This is a gas-optimized method for storing the job data on-chain.
+            *
+            * See executeJob() to see how the on-chain job data is consumed.
+         */
         uint jobId = hashJob(target, value, calldatas, msg.sender);
 
         // create job
@@ -49,16 +65,27 @@ contract QuestionH {
         j.bounty = msg.value;
         j.executeAt = block.timestamp + delay;
 
+        /**
+            * NOTE: executeJob() below requires the same params used to submit a job
+            * in order to find the job's id.
+            *
+            * I'm not sure what best practice here would be. These values could be stored
+            * in a centralized DB or decentralized storage solution from the application layer.
+            * This solution would be more gas-efficient but an error here could make the given
+            * job's params difficult to retrieve.
+         */
         emit JobSubmitted(msg.sender, jobId, target, value, calldatas, delay);
     }
 
     /**
-        Spec
-        * Caller can execute the given job if the job has not already been executed
-        * Caller will receive the bounty associated with the job
-
-        Params
+        * @dev Execute a submitted job
+        *
+        * Requirements:
+        * - Given params have to correspond to a previously submitted job.
+        * - Caller can only execute a job if the job's delay has expired.
+        * - Caller can only execute the given job if the job has not already been executed.
         * 
+        * Caller will receive the bounty associated with the job.
     */
     function executeJob(address target, uint value, bytes calldatas, uint delay) external {
         uint jobId = hashJob(target, value, calldatas, msg.sender);
@@ -67,8 +94,12 @@ contract QuestionH {
         require(j.id > 0, "QuestionH::executeJob: INVALID_JOB");
         require(block.timestamp >= j.executeAt, "QuestionH::executeJob: JOB_INACTIVE");
         require(!j.executed, "QuestionH::executeJob: JOB_ALREADY_EXECUTED");
+        // This should prevent reentrancy attacks
+        require(!executing, "QuestionH:executeJob: CURRENTLY_EXECUTING");
 
+        // Second protection against reentrancy attacks
         j.executed = true;
+        executing = true;
 
         (bool success, bytes memory returndata) = target.call{value: value}(calldatas);
 
@@ -86,5 +117,7 @@ contract QuestionH {
             // No revert reason given
             revert("Call reverted without message");
         }
+
+        executing = false;
     }    
 }
